0e58ec4baff7fe388379da148fd20306deade8a1
swetha <155046919+SwEtHaRaViNdRa@users.noreply.github.com>
2024-08-15 21:23:38 +0530

    Add files via upload

---
+++ rel-oper.py
@@ -0,0 +1,14 @@
+a=int(input("first:"))
+b=int(input("second:"))
+print("*"*50)
+print("\t Result")
+print("*"*50)
+print("\t\t{}>{}={}".format(a,b,a>b))
+print("\t\t{}<{}={}".format(a,b,a<b))
+print("\t\t{}=={}={}".format(a,b,a==b))
+print("\t\t{}>={}={}".format(a,b,a>=b))
+print("\t\t{}<={}={}".format(a,b,a<=b))
+print("\t\t{}!={}={}".format(a,b,a!=b))
+print("*"*50)
+
+
